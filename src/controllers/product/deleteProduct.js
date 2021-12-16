4e233eeb9c0f8fea2d17f0c30d9cdde1f6f2cff4
edwardngo <hoangso8000@gmail.com>
2021-12-16 23:07:43 +0700

    Modify req body to params

--- src/controllers/product/deleteProduct.js
+++ src/controllers/product/deleteProduct.js
@@ -2,13 +2,15 @@ const { abort } = require('../../utils/errors');
 const Product = require('../../models/Product');
 
 async function deleteProduct(req,res){
-    const productId = req.body.id
-    try {
-        const response = await Product.destroy({where:{id: productId}})
-    } catch(err){
-        
+    const productId = req.params.id
+    const response = await Product.destroy({where:{id: productId}})
+    if (response){
+        res.status(201).send("Delete Product successfully")
+    } else{
+        res.status(404).send({
+            message: `Cannot find Product with id=${productId}.`
+          });
     }
-    return res.status(201).send()
 }
 
 module.exports = deleteProduct
