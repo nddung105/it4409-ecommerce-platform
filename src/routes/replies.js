e4a13a0a1455e739e49f0ee65abb1ba51f462e22
edwardngo <hoangso8000@gmail.com>
2022-01-14 21:18:57 +0700

    Resolved conflic

--- src/routes/replies.js
+++ src/routes/replies.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router();
-const auth = require("../middlewares/auth");
+const {validUser,validJWTAdmin} = require("../middlewares/validate");
+const auth = require("../middlewares/auth")
 const {asyncWrapper} = require("../utils/asyncWrapper")
 
 const {reply:replyController} = require('../controllers')
 
-router.post('/',replyController.replyComment)
-router.put('/',replyController.modifyReply)
+router.post('/',auth,replyController.replyComment)
+router.put('/:reply_id',validJWTAdmin,replyController.modifyReply)
 module.exports = router
