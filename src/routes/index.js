a3b3c03f9d02eb3cb91b60c71135fd2b64a30210
nddung105 <dungnguyendinh1052000@gmail.con>
2022-01-14 10:43:04 +0700

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,18 @@ const apiRouter = express.Router();
 
 // apiRouter.use("/api/v1/", mainRoute);
 apiRouter.use("/files/", express.static(path.join(__dirname, "../files")));
-apiRouter.use("/", express.static(path.join(__dirname, "../views")));
+// apiRouter.use("/", express.static(path.join(__dirname, "../views")));
+apiRouter.use("/customer", function(req, res, next ){
+    if (req.headers.authorization){
+        console.log(req.headers.authorization)
+        next()
+    }
+    else{
+        res.status(404).json({
+            success: false,
+            message: "unauthorized",
+          });
+    }
+},express.static(path.join(__dirname, "../views/customer")))
 
 module.exports = apiRouter;
