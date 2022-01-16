34e4bfd2b9ce48a81449b3a51aaa1bea3ecf01a1
nddung105 <dungnguyendinh1052000@gmail.con>
2022-01-16 12:24:47 +0700

    API Order: Sales by month

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { validJWTSale, validJWT } = require("../middlewares/validate");
+const { validJWTSale, validJWT, validJWTAdmin } = require("../middlewares/validate");
 const auth = require("../middlewares/validate");
 const { order: orderController } = require("../controllers");
 
@@ -8,6 +8,7 @@ router.post("/add", validJWT, orderController.add);
 router.post("/edit", validJWT, orderController.edit);
 router.post("/sale_edit", validJWTSale, orderController.sale_edit);
 router.get("/show", validJWTSale, orderController.show);
+router.get("/sales_by_month", validJWTAdmin, orderController.sales_by_month);
 router.get("/show_all_order_user", validJWT, orderController.show_all_order_user);
 router.get("/:order_id", validJWT, orderController.get);
 
