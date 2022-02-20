c7cd7a34d06a9369683b88422761a0a0689bbb96
Nguyen Kim Tung <72078935+tungnk181120@users.noreply.github.com>
2022-02-20 20:54:40 +0700

    update customer routes

--- index.js
+++ index.js
@@ -15,19 +15,50 @@ app.use(express.json());
 // use middleware to enable cors
 app.use(cors());
 
-//route init
-app.use('/', apiRouter)
-
+//customer router
+//index
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/src/views/index.html');
-  });
-
+    res.sendFile(__dirname + '/src/views/customer/index.html');
+});
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + '/src/views/customer/login.html');
+});
+app.get('/sign-up', function (req, res) {
+    res.sendFile(__dirname + '/src/views/customer/sign-up.html');
+});
+//products
+app.get('/products', function (req, res) {
+    res.sendFile(__dirname + '/src/views/customer/product.html');
+});
 app.get('/products/:id', function (req, res) {
     res.sendFile(__dirname + '/src/views/customer/product-detail.html');
 });
 app.get('/comment/:id', function (req, res) {
     res.sendFile(__dirname + '/src/views/customer/reviewing.html');
 }); 
+app.get('/cart', function (req, res) {
+    res.sendFile(__dirname + '/src/views/customer/cart.html');
+});
+//user
+app.get('/user_info', function (req, res) {
+    res.sendFile(__dirname + '/src/views/customer/user_info.html');
+});
+app.get('/history_transaction', function (req, res) {
+    res.sendFile(__dirname + '/src/views/customer/history_transaction.html');
+});
+app.get('/payment', function (req, res) {
+    res.sendFile(__dirname + '/src/views/customer/payment.html');
+});
+app.get('/payment-success', function (req, res) {
+    res.sendFile(__dirname + '/src/views/customer/payment-success.html');
+});
+
+//Admin router
+
+//route api init
+app.use('/', apiRouter)
+
+
 app.use(express.static(path.join(__dirname, '/src/public')));
 
 app.listen(PORT, () => {
